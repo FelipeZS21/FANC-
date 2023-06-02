f60d4f72b0d7335d0ed2d4ac216ccd3dab48347d
FelipeZS21 <felipezasie@hotmail.com>
2023-06-01 22:25:23 -0500

    arreglando categorias

--- fact-page/src/components/Categorias/Categorias.jsx
+++ fact-page/src/components/Categorias/Categorias.jsx
@@ -20,32 +20,28 @@ const Categorias = () =>{
     return(
         <div className="principal">
             <div className="box-text-category">
-                <h2>Categorias preferidas: </h2>
+                <h2>Categorias preferidas </h2>
             </div>
             <div className="principal-container-category">
-                <div className="box-container-category">
+                <div className="box-container-category box1">
                     <img src="../../Public/Imgs/aventura.png" alt="" />
                     <h2>Aventura</h2>
                 </div>
-                <div className="box-container-category">
+                <div className="box-container-category box2">
                     <img src="../../Public/Imgs/accion.png" alt="" />
                     <h2>Accion</h2>
                 </div>
-                <div className="box-container-category">
+                <div className="box-container-category box3">
                     <img src="../../Public/Imgs/ficcion.png" alt="" />
                     <h2>Ficcion</h2>
                 </div>
-                <div className="box-container-category">
+                <div className="box-container-category box4">
                     <img src="../../Public/Imgs/terror.png" alt="" />
                     <h2>Terror</h2>
                 </div>
-                <div className="box-container-category">
+                <div className="box-container-category box5">
                     <img src="../../Public/Imgs/romance.png" alt="" />
                     <h2>Romance</h2>
-                </div>
-                <div className="box-container-category">
-                    <img src="../../Public/Imgs/psicologia.png" alt="" />
-                    <h2>Psicologia</h2>
                 </div>
             </div>
         </div>
